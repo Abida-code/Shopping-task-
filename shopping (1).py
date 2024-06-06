c565f167d55eadba1665008ff1769e7f725c82b8
Abida-code <76618402+Abida-code@users.noreply.github.com>
2024-06-06 14:59:16 +0300

    Add files via upload

---
+++ shopping (1).py
@@ -0,0 +1,76 @@
+import csv
+import sys
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+TEST_SIZE = 0.4
+
+def main():
+
+    evidence, labels = load_data("shopping.csv")
+    X_train, X_test, y_train, y_test = train_test_split(
+        evidence, labels, test_size=TEST_SIZE
+    )
+
+    model = train_model(X_train, y_train)
+    predictions = model.predict(X_test)
+    accuracy, precision, recall, f1 = evaluate(y_test, predictions)
+
+    print(f"Correct: {(y_test == predictions).sum()}")
+    print(f"Incorrect: {(y_test != predictions).sum()}")
+    print(f"Accuracy: {100 * accuracy:.2f}%")
+    print(f"Precision: {100 * precision:.2f}%")
+    print(f"Recall: {100 * recall:.2f}%")
+    print(f"F1-score: {100 * f1:.2f}%")
+
+def load_data(filename):
+    month_to_num = {
+        "Jan": 0, "Feb": 1, "Mar": 2, "Apr": 3, "May": 4, "June": 5,
+        "Jul": 6, "Aug": 7, "Sep": 8, "Oct": 9, "Nov": 10, "Dec": 11
+    }
+    evidence = []
+    labels = []
+
+    with open(filename) as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            row_evidence = [
+                int(row["Administrative"]),
+                float(row["Administrative_Duration"]),
+                int(row["Informational"]),
+                float(row["Informational_Duration"]),
+                int(row["ProductRelated"]),
+                float(row["ProductRelated_Duration"]),
+                float(row["BounceRates"]),
+                float(row["ExitRates"]),
+                float(row["PageValues"]),
+                float(row["SpecialDay"]),
+                month_to_num[row["Month"]],
+                int(row["OperatingSystems"]),
+                int(row["Browser"]),
+                int(row["Region"]),
+                int(row["TrafficType"]),
+                1 if row["VisitorType"] == "Returning_Visitor" else 0,
+                1 if row["Weekend"] == "TRUE" else 0
+            ]
+            evidence.append(row_evidence)
+            labels.append(1 if row["Revenue"] == "TRUE" else 0)
+    
+    return evidence, labels
+
+def train_model(evidence, labels):
+    model = KNeighborsClassifier(n_neighbors=1)
+    model.fit(evidence, labels)
+    return model
+
+def evaluate(labels, predictions):
+    accuracy = accuracy_score(labels, predictions)
+    precision = precision_score(labels, predictions, average='weighted')
+    recall = recall_score(labels, predictions, average='weighted')
+    f1 = f1_score(labels, predictions, average='weighted')
+    
+    return accuracy, precision, recall, f1
+
+if __name__ == "__main__":
+    main()
